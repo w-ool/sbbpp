8e0d84340b336ca1bbb44a0f3f9e042939927342
wool <docus17@gmail.com>
2023-08-12 16:04:55 +0900

    수정, 삭제

--- sbbpp/src/main/java/com/site/sbbpp/answer/AnswerService.java
+++ sbbpp/src/main/java/com/site/sbbpp/answer/AnswerService.java
@@ -1,12 +1,14 @@
 package com.site.sbbpp.answer;
 
-import com.site.sbbpp.question.Question;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+
+import org.springframework.stereotype.Service;import java.util.Optional;
 
 import java.time.LocalDateTime;
 
+import com.site.sbbpp.question.Question;
 import com.site.sbbpp.user.SiteUser;
+import com.site.sbbpp.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -23,5 +25,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
