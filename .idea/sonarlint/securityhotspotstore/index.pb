cf24e8220539bcb4d4cfc4657778e0080c91d240
w-ool <docus17@gmail.com>
2023-08-14 17:36:38 +0900

    검색

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,8 +3,6 @@ Q
 !sbbpp/.gradle/8.1.1/gc.properties,a\6\a6519d0bffe8b327edbdd711cfc12d63cf67a5cb
 _
 /sbbpp/src/main/resources/application.properties,e\8\e8a478319de862c67794586ef97e5cee15dc7cdd
-s
-Csbbpp/src/main/java/com/site/sbbpp/question/QuestionRepository.java,8\1\81b2093fbb225e2ca1ddbc51e715785ee9e3781e
 e
 5sbbpp/src/main/java/com/site/sbbpp/user/SiteUser.java,a\a\aabaf871452b82dff9641913e2a068454c7d4693
 k
--- sbbpp/src/main/java/com/site/sbbpp/question/QuestionService.java
+++ sbbpp/src/main/java/com/site/sbbpp/question/QuestionService.java
@@ -2,17 +2,26 @@ package com.site.sbbpp.question;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.data.domain.Sort;
 import java.util.Optional;
 import java.time.LocalDateTime;
 
 import com.site.sbbpp.DataNotFoundException;
 import com.site.sbbpp.user.SiteUser;
+import com.site.sbbpp.answer.Answer;
 
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +30,24 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
+    }
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
@@ -31,11 +58,12 @@ public class QuestionService {
         }
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
 
     public void create(String subject, String content, SiteUser user) {
